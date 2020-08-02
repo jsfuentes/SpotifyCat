9e46e1f3ede6a3de539b0c057bdea2c17afde5f9
Jorge Fuentes <jsjfuentesj@gmail.com>
2020-08-02 02:48:55 -0700

    Update client urls

--- assets/src/conf/production.js
+++ assets/src/conf/production.js
@@ -1,5 +1,5 @@
 export default {
   NAME: "React Prod",
-  SERVER_URL: "https://mysterious-tor-16357.herokuapp.com",
-  CLIENT_URL: "https://mysterious-tor-16357.herokuapp.com",
+  SERVER_URL: "https://spotify-cat.herokuapp.com",
+  CLIENT_URL: "https://spotify-cat.herokuapp.com",
 };
